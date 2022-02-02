8c1f25e49af0bf17466ae409c092d6a1d7c45887
RibaRibka_macMini <abykov1@jh.edu>
2022-02-02 13:50:43 -0500

    Analysis of TFs from pha-4 list

--- pha-4_tagets_comparison.R
+++ pha-4_tagets_comparison.R
@@ -64,7 +64,7 @@ ms_new <- subset(ms, idents =c("MSxpp","MSxapa","MSxap", "MSxp",  "MSxpa", "MSxa
 pha4_targets <- read.delim('./pha-4_genes_list_embryos_snyder_plos_gen_2010.txt',header = FALSE)
 pha4_targets <- pha4_targets[,1]
 
-
+# just subsetting data, splitting into three groups
 ms_new_MSxaax <- subset(ms_new, idents = c('MSxaaa', 'MSxaap', 'MSxapa'))
 ms_new_MSxaax$ident <- ms_new_MSxaax@active.ident
 
@@ -84,53 +84,55 @@ ms_new_MSxaax_expr_data
 
 
 pha4_targets
-
+# trying to range genes by they mean expression value and leave only the ones that are present in the target list. Not really using this later.
 ms_new_MSxaax_expr_data <- ms_new_MSxaax_expr_data %>% filter(row.names(ms_new_MSxaax_expr_data) %in% pha4_targets)
-
-
 ms_new_MSxaax_expr_data <- arrange(ms_new_MSxaax_expr_data, desc(mean))
 ms_new_MSxaax_expr_data[1:1000,]
 
 DoHeatmap(ms_new_MSxaax, features = rownames(ms_new_MSxaax_expr_data)[1:50],slot = 'data')
 
+
+
 #########################################################
 # can try to discard the genes that are expressed in less than 50% of cells per cluster
-
-# For MSxapa
-MSxapa_gene_name_space <- list()
+# For MSxaax
+MSxaax_gene_name_space <- list()
 for (cluster_ident_name in levels(Idents(ms_new_MSxaax))) {
   ms_tmp<- subset(ms_new_MSxaax, idents = c(cluster_ident_name))
   df_tmp <- as.data.frame(ms_tmp@assays$RNA@data)
   percent_not_zero <- rowSums(df_tmp!=0)/ncol(df_tmp)*100 # percent of cells that expression is not 0
   tmp_list <- list(names(percent_not_zero)[percent_not_zero > 50]) # names of genes that are expressed in more than 50% of cells
   names(tmp_list) <- cluster_ident_name
-  MSxapa_gene_name_space <- c(MSxapa_gene_name_space,tmp_list)
+  MSxaax_gene_name_space <- c(MSxaax_gene_name_space,tmp_list)
 }
-MSxapa_gene_name_space
-library(VennDiagram)
-venn.diagram(MSxapa_gene_name_space,output=TRUE, filename = "test.png") # look into the working folder - the image is there
+MSxaax_gene_name_space
+#library(VennDiagram)
+#venn.diagram(MSxaax_gene_name_space,output=TRUE, filename = "test.png") # look into the working folder - the image is there
 
 
 
-############
+####
 # try to filter the TFs
 # the list of genes is from https://genomebiology.biomedcentral.com/articles/10.1186/gb-2005-6-13-r110#MOESM1 
 # A compendium of Caenorhabditis elegans regulatory transcription factors: a resource for mapping transcription regulatory networks
-
-
 tflist <- (read.delim('./all_TFs_celegans.csv', header = FALSE))[,1]
 
 # All TFs combined in the lineages.
-MSxapa_gene_list <- unique(c(
-intersect(MSxapa_gene_name_space[[1]], tflist),
-intersect(MSxapa_gene_name_space[[2]], tflist),
-intersect(MSxapa_gene_name_space[[3]], tflist)))
-DoHeatmap(ms_new_MSxaax, features = MSxapa_gene_list, slot = 'data')
+MSxaax_gene_list <- unique(c(
+intersect(MSxaax_gene_name_space[[1]], tflist),
+intersect(MSxaax_gene_name_space[[2]], tflist),
+intersect(MSxaax_gene_name_space[[3]], tflist)))
 
-# Intersect of the genes
-MSxapa_gene_list <- intersect(intersect(MSxapa_gene_name_space[[1]], MSxapa_gene_name_space[[2]]),MSxapa_gene_name_space[[3]])
-MSxapa_gene_list <- intersect(MSxapa_gene_list, tflist)
-DoHeatmap(ms_new_MSxaax, features = MSxapa_gene_list, slot = 'data')
+# Not so much sense to use it - since we assume that the core genes, regulated by pha-4 are the same in this group of cells.
+DoHeatmap(ms_new_MSxaax, features = MSxaax_gene_list, slot = 'data')
+
+# Intersect of the genes with pha-4 target list
+# Here I try to use only TF's, cz if I use all genes, the list is very long - 730 genes, or so. Too many.
+MSxaax_gene_list <- intersect(intersect(MSxaax_gene_name_space[[1]], MSxaax_gene_name_space[[2]]),MSxaax_gene_name_space[[3]])
+MSxaax_gene_list <- intersect(MSxaax_gene_list, tflist)
+MSxaax_gene_list <- intersect(MSxaax_gene_list, pha4_targets)
+
+DoHeatmap(ms_new_MSxaax, features = MSxaax_gene_list, slot = 'data')
 
 
 
@@ -154,7 +156,8 @@ MSxapp_gene_name_space
 tflist <- (read.delim('./all_TFs_celegans.csv', header = FALSE))[,1]
 # All TFs combined in the lineages.
 MSxapp_gene_list <- intersect(MSxapp_gene_name_space[[1]], tflist)
-  
+MSxapp_gene_list <- intersect(MSxapp_gene_list, pha4_targets)
+
 DoHeatmap(ms_new_MSxapp, features = MSxapp_gene_list, slot = 'data')
 
 
@@ -196,11 +199,54 @@ DoHeatmap(ms_new_MSxpxx, features = MSxpxx_gene_list, slot = 'data')
 # Intersect of the genes
 MSxpxx_gene_list <- intersect(MSxpxx_gene_name_space[[4]],intersect(intersect(MSxpxx_gene_name_space[[1]], MSxpxx_gene_name_space[[2]]),MSxpxx_gene_name_space[[3]]))
 MSxpxx_gene_list <- intersect(MSxpxx_gene_list, tflist)
+MSxpxx_gene_list <- intersect(MSxpxx_gene_list, pha4_targets)
 DoHeatmap(ms_new_MSxpxx, features = MSxpxx_gene_list, slot = 'data')
 
 
 
 
+#########################################################
+# identifying the genes that are ubiquitously expressed in all cells and removing them from those datasets.
+
+
+MSxaax_gene_list
+MSxapp_gene_list
+MSxpxx_gene_list
+
+# visualise some genes distributions:
+library(VennDiagram)
+tmp_list <- list(MSxaax_gene_list, MSxapp_gene_list, MSxpxx_gene_list)
+names(tmp_list) <- c("MSxaax_gene_list", "MSxapp_gene_list", "MSxpxx_gene_list")
+venn.diagram(tmp_list,output=TRUE, filename = "test.png", resolution = 300)
+
+
+# COmparing Msxaxx and MSxapp
+# there is a big intersect between MSxaxx and MSxapp expressed genes:
+intersect(MSxaax_gene_list,MSxapp_gene_list)
+  # genes unique to MSxapa in comparison to the MSxapp:
+setdiff(MSxaax_gene_list, MSxapp_gene_list)
+  # Genes that are unique to MSxapp in comparison to MSxaxx:
+setdiff( MSxapp_gene_list,MSxaax_gene_list)
+
+
+
+# COmparing Msxpxx and MSxapp
+# there is a big intersect between MSxaxx and MSxapp expressed genes:
+intersect(MSxpxx_gene_list,MSxapp_gene_list)
+# genes unique to MSxpxx in comparison to the MSxapp:
+setdiff(MSxpxx_gene_list, MSxapp_gene_list)
+# Genes that are unique to MSxapp in comparison to MSxpxx:
+setdiff(MSxapp_gene_list, MSxpxx_gene_list)
+
+
+
+# COmparing Msxpxx and MSxaxx
+# there is a big intersect between MSxaxx and MSxapp expressed genes:
+intersect(MSxpxx_gene_list,MSxaax_gene_list)
+# genes unique to MSxpxx in comparison to the MSxaxx:
+setdiff(MSxpxx_gene_list, MSxaax_gene_list)
+# Genes that are unique to MSxaxx in comparison to MSxpxx:
+setdiff(MSxaax_gene_list, MSxpxx_gene_list)
 
 
 
@@ -208,6 +254,139 @@ DoHeatmap(ms_new_MSxpxx, features = MSxpxx_gene_list, slot = 'data')
 
 
 
+
+#################################################################################################################################################
+# The same, but now with late timepoints
+
+
+# just subsetting data, splitting into three groups
+#
+# All avaliable pharyngeal cells. But we want to use muscle cells
+# ms_new_MSxaxxx_late <- subset(ms_new, idents = c('MSaaaaaaa', 'MSaaaaapa', 
+#                                                  'MSaaaapa','MSaaaappp/MSxapaapp', 
+#                                                  'MSpaaappp/MSxapappa', 'MSaaapaa', 'MSpaapaa',
+#                                                  'MSxaapapx', 'MSaaappp','MSxapaaa',
+#                                                  'MSxapaapa','MSxapapa.MSxapapaa',
+#                                                  'MSxapappp.like'))
+
+ms_new_MSxaxxx_late <- subset(ms_new, idents = c('MSaaapaa','MSxaapapx','MSaaaappp/MSxapaapp','MSxapaaa','MSpaaappp/MSxapappa'))
+
+ms_new_MSxaxxx_late$ident <- ms_new_MSxaxxx_late@active.ident
+
+ms_new_MSxapp_late <- subset(ms_new, idents = c('MSxappppx','MSxapppax'))
+ms_new_MSxapp_late$ident <- ms_new_MSxapp_late@active.ident
+
+ms_new_MSxpxx_late <- subset(ms_new, idents = c('MSxppppp', 'MSxppppa', 'MSxpppap', 'MSxpppaa',
+                                                'MSxppapp', 'MSxpappp', 'MSxpappa','MSxpapap',
+                                                'MSxpaaap'))
+ms_new_MSxpxx_late$ident <- ms_new_MSxpxx_late@active.ident
+
+
+
+######
+MSxaxxx_late_gene_name_space <- list()
+for (cluster_ident_name in levels(Idents(ms_new_MSxaxxx_late))) {
+  ms_tmp<- subset(ms_new_MSxaxxx_late, idents = c(cluster_ident_name))
+  df_tmp <- as.data.frame(ms_tmp@assays$RNA@data)
+  percent_not_zero <- rowSums(df_tmp!=0)/ncol(df_tmp)*100 # percent of cells that expression is not 0
+  tmp_list <- list(names(percent_not_zero)[percent_not_zero > 50]) # names of genes that are expressed in more than 50% of cells
+  names(tmp_list) <- cluster_ident_name
+  MSxaxxx_late_gene_name_space <- c(MSxaxxx_late_gene_name_space,tmp_list)
+}
+MSxaxxx_late_gene_name_space
+#library(VennDiagram)
+#venn.diagram(MSxaax_gene_name_space,output=TRUE, filename = "test.png") # look into the working folder - the image is there
+
+
+
+####
+# try to filter the TFs
+# the list of genes is from https://genomebiology.biomedcentral.com/articles/10.1186/gb-2005-6-13-r110#MOESM1 
+# A compendium of Caenorhabditis elegans regulatory transcription factors: a resource for mapping transcription regulatory networks
+tflist <- (read.delim('./all_TFs_celegans.csv', header = FALSE))[,1]
+
+# filter only pha-4 targets
+MSxaxxx_late_gene_list <- intersect(unique(unlist(MSxaxxx_late_gene_name_space)), pha4_targets)
+
+DoHeatmap(ms_new_MSxaxxx_late, features = MSxaxxx_late_gene_list, slot = 'data')
+
+MSxaxxx_late_gene_list_tfs <- intersect(MSxaxxx_late_gene_list, tflist)
+# Intersect of the genes with pha-4 target list
+# Here I try to use only TF's, cz if I use all genes, the list is very long - 730 genes, or so. Too many.
+
+DoHeatmap(ms_new_MSxaxxx_late, features = MSxaxxx_late_gene_list_tfs, slot = 'data')
+
+
+
+
+
+
+#########################################################
+# For ms_new_MSxapp_late
+######
+MSxapp_late_gene_name_space <- list()
+for (cluster_ident_name in levels(Idents(ms_new_MSxapp_late))) {
+  ms_tmp<- subset(ms_new_MSxapp_late, idents = c(cluster_ident_name))
+  df_tmp <- as.data.frame(ms_tmp@assays$RNA@data)
+  percent_not_zero <- rowSums(df_tmp!=0)/ncol(df_tmp)*100 # percent of cells that expression is not 0
+  tmp_list <- list(names(percent_not_zero)[percent_not_zero > 50]) # names of genes that are expressed in more than 50% of cells
+  names(tmp_list) <- cluster_ident_name
+  MSxapp_late_gene_name_space <- c(MSxapp_late_gene_name_space,tmp_list)
+}
+MSxapp_late_gene_name_space
+
+####
+# try to filter the TFs
+# the list of genes is from https://genomebiology.biomedcentral.com/articles/10.1186/gb-2005-6-13-r110#MOESM1 
+# A compendium of Caenorhabditis elegans regulatory transcription factors: a resource for mapping transcription regulatory networks
+tflist <- (read.delim('./all_TFs_celegans.csv', header = FALSE))[,1]
+
+# filter only pha-4 targets
+MSxapp_late_gene_list <- intersect(unique(unlist(MSxapp_late_gene_name_space)), pha4_targets)
+
+DoHeatmap(ms_new_MSxapp_late, features = MSxapp_late_gene_list, slot = 'data')
+
+MSxapp_late_gene_list_tfs <- intersect(MSxapp_late_gene_list, tflist)
+DoHeatmap(ms_new_MSxapp_late, features = MSxaxxx_late_gene_list_tfs, slot = 'data')
+
+
+
+#########################################################
+# For ms_new_MSxpxx_late
+######
+MSxpxx_late_gene_name_space <- list()
+for (cluster_ident_name in levels(Idents(ms_new_MSxpxx_late))) {
+  ms_tmp<- subset(ms_new_MSxpxx_late, idents = c(cluster_ident_name))
+  df_tmp <- as.data.frame(ms_tmp@assays$RNA@data)
+  percent_not_zero <- rowSums(df_tmp!=0)/ncol(df_tmp)*100 # percent of cells that expression is not 0
+  tmp_list <- list(names(percent_not_zero)[percent_not_zero > 50]) # names of genes that are expressed in more than 50% of cells
+  names(tmp_list) <- cluster_ident_name
+  MSxpxx_late_gene_name_space <- c(MSxpxx_late_gene_name_space,tmp_list)
+}
+MSxpxx_late_gene_name_space
+
+####
+# try to filter the TFs
+# the list of genes is from https://genomebiology.biomedcentral.com/articles/10.1186/gb-2005-6-13-r110#MOESM1 
+# A compendium of Caenorhabditis elegans regulatory transcription factors: a resource for mapping transcription regulatory networks
+tflist <- (read.delim('./all_TFs_celegans.csv', header = FALSE))[,1]
+
+# filter only pha-4 targets
+MSxpxx_late_gene_list <- intersect(unique(unlist(MSxpxx_late_gene_name_space)), pha4_targets)
+
+DoHeatmap(ms_new_MSxpxx_late, features = MSxpxx_late_gene_list, slot = 'data')
+
+MSxpxx_late_gene_list_tfs <- intersect(MSxpxx_late_gene_list, tflist)
+DoHeatmap(ms_new_MSxpxx_late, features = MSxpxx_late_gene_list_tfs, slot = 'data')
+
+
+
+
+
+
+MSxaxxx_late_gene_list_tfs
+MSxapp_late_gene_list_tfs
+MSxpxx_late_gene_list_tfs
 
 
 
